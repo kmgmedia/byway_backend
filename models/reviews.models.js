d273fcedabcec2ace56aec1e42ad3ae046df072f
Moruf <hellokmgmedia@gmail.com>
2025-06-23 10:55:49 +0100

    Feat: implement versel config file for serverless function

--- models/reviews.models.js
+++ models/reviews.models.js
@@ -1,23 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const reviewSchema = new mongoose.Schema(
-//   {
-//     title: { type: String, required: true, maxlength: 100 },
-//     reviewDetails: { type: String, required: true },
-//     reviewImage: { type: String },
-//     reviewText: { type: String, required: true, minlength: 10 },
-//     reviewMore: { type: String, required: true },
-//     reviewDate: { type: Date, default: Date.now },
-//     rating: { type: Number, required: true, min: 1, max: 5 },
-//   },
-//   {
-//     timestamps: true,
-//   }
-// );
-
-// module.exports = mongoose.model("Review", reviewSchema);
-
-
 const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema(
